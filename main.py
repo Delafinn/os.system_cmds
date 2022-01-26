bbe6732bfde2c21b0cfdd8cb88bd3dc49aa46cf5
Finn <32446445+Delafinn@users.noreply.github.com>
2022-01-25 19:19:22 -0700

    main.py
    refactored code

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # this is a automated terminal cli for basic terminal or cli
 
 import os
-import time
 import sys
 
-commands = {"exit":"exit the terminal" ,"ip" : "show your ip address in terminal",
- "clear" : "clear the terminal", "hostname" : "display your computer name", "top": "display all running programs on your computer" ,
+commands = {"exit":"exit the terminal" ,
+"ip" : "show your ip address in terminal",
+ "clear" : "clear the terminal",
+  "hostname" : "display your computer name",
+  "top": "display all running programs on your computer" ,
  "systems" : "pulls system information on macos or windows."
  }
-
-
-def exit():
-    terminal = "exit"
-    return os.system(terminal)
 
 
 def ipconfig():
@@ -40,40 +37,42 @@ def hostname():
 def top():
     terminal = "top" # if on mac
     if os.name in ("nt"):# if on windows
-         terminal = "tasklist"
-         return os.system(terminal)
+        terminal = "tasklist"
+        return os.system(terminal)
     return os.system(terminal)
 
 def systems():
-    terminal = "system_profiler -detailLevel mini" #if on mac
-    if os.name in ("nt"): # if on windows
+    terminal = "system_profiler -detailLevel mini"
+    if os.name in ("nt"):
         terminal = "systeminfo"
         return os.system(terminal)
-    elif sys.platform.startswith("linux"): # if on linxu
-        terminal = "lscpu && uname -a"
+    elif sys.platform.startswith("linux"):
+        terminal = "sudo lshw -short && lscpu"
         return os.system(terminal)
     else:
         return os.system(terminal)
 
 # main program
 print(commands)
-command_question = input("what function/command do you want to call? \n NOTE!!! you can call a command by just the first letter of the displayed command or you can type the matching word.").lower()
-if command_question == "exit":
-    exit()
+command_question = input("""what function/command do you want to call?
+\n NOTE!!! you can call a command by just the first letter of the displayed
+command or you can type the matching word.""")
+if command_question in "exit":
+    sys.exit()
 
-elif command_question == "ip" or command_question == "i" :
+elif command_question in ("ip" , "i") :
     ipconfig()
 
-elif command_question == "clear" or command_question == "c" :
+elif command_question in ("clear" , "c") :
     clear()
 
-elif command_question == "hostname" or command_question == "h" :
+elif command_question in ("hostname" , "h") :
     hostname()
 
-elif command_question == "top" or command_question == "t":
+elif command_question in ("top" , "t"):
     top()
 
-elif command_question == "systems" or command_question == "s":
+elif command_question in ("systems" , "s"):
     systems()
 
 else:
