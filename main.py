b1a486edb929323015d9abfbded1bfe62eb19019
Finn <32446445+Delafinn@users.noreply.github.com>
2022-03-08 18:56:34 -0700

    main.py
    added a while loop to the main program

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # this is a automated terminal cli for basic terminal or cli
 
 import os
+import time
 import sys
 
-commands = {"exit":"exit the terminal" ,
-"ip" : "show your ip address in terminal",
- "clear" : "clear the terminal",
-  "hostname" : "display your computer name",
-  "top": "display all running programs on your computer" ,
+commands = {"exit":"exit the terminal" ,"ip" : "show your ip address in terminal",
+ "clear" : "clear the terminal", "hostname" : "display your computer name", "top": "display all running programs on your computer" ,
  "systems" : "pulls system information on macos or windows."
  }
+
+
+def exit():
+    terminal = "exit"
+    return os.system(terminal)
 
 
 def ipconfig():
@@ -37,8 +40,8 @@ def hostname():
 def top():
     terminal = "top" # if on mac
     if os.name in ("nt"):# if on windows
-        terminal = "tasklist"
-        return os.system(terminal)
+         terminal = "tasklist"
+         return os.system(terminal)
     return os.system(terminal)
 
 def systems():
@@ -53,27 +56,32 @@ def systems():
         return os.system(terminal)
 
 # main program
-print(commands)
-command_question = input("""what function/command do you want to call?
-\n NOTE!!! you can call a command by just the first letter of the displayed
-command or you can type the matching word.""")
-if command_question in "exit":
-    sys.exit()
+while True:
 
-elif command_question in ("ip" , "i") :
-    ipconfig()
+    print(commands)
+    command_question = input("what function/command do you want to call? \n NOTE!!! you can call a command by just the first letter of the displayed command or you can type the matching word.").lower()
+    if command_question == "exit":
+        break
 
-elif command_question in ("clear" , "c") :
-    clear()
+    elif command_question == "ip" or command_question == "i" :
+        ipconfig()
+        time.sleep(4)
 
-elif command_question in ("hostname" , "h") :
-    hostname()
+    elif command_question == "clear" or command_question == "c" :
+        clear()
 
-elif command_question in ("top" , "t"):
-    top()
+    elif command_question == "hostname" or command_question == "h" :
+        hostname()
+        time.sleep(4)
 
-elif command_question in ("systems" , "s"):
-    systems()
+    elif command_question == "top" or command_question == "t":
+        top()
+        time.sleep(4)
 
-else:
-    print("INVALID COMMAND!")
+    elif command_question == "systems" or command_question == "s":
+        systems()
+        time.sleep(4)
+
+    else:
+        print("INVALID COMMAND!")
+        time.sleep(4)
